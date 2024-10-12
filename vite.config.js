9c0fecdc369e41816b36acfa7b60145f4add526c
Jorge Herrera <jorgeluishm88@gmail.com>
2024-10-12 16:46:15 -0300

    undo modificação arquivo vite

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,4 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-  server: {
-    historyApiFallback: true,
-  },
-});
+export default defineConfig({});
